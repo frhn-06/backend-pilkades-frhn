942cda87a44780bf6972c43332b1afec1064bb64
frhn-06 <farhanmunif233@gmail.com>
2026-07-07 09:41:33 +0700

    change prisma client generator

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "../generated/prisma/client";
+// import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@prisma/client/extension";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { DATABASE_URL } from "../utils/env";
 
