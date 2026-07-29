f2cd727e877aa3e5caca12afce74a1b4b3b19dc3
frhn-06 <farhanmunif233@gmail.com>
2026-07-29 10:48:39 +0700

    fix:nullable  pada validasi voter

--- src/validations/voter.validation.ts
+++ src/validations/voter.validation.ts
@@ -2,8 +2,8 @@ import zod, {z} from 'zod';
 
 const voterDTO = zod.object({
     name: z.string().trim().min(2).max(100),
-    nik: z.string().trim().max(16).optional(),
-    info: z.string().trim().max(100).optional()
+    nik: z.string().trim().max(16).nullable().optional(),
+    info: z.string().trim().max(100).nullable().optional()
 });
 
 export {voterDTO};
