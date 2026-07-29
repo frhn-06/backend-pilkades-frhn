3048fd12d403dfa972241a3e42b7cd3e0cf16346
frhn-06 <farhanmunif233@gmail.com>
2026-07-29 10:36:13 +0700

    fix:hilangkan min pada validasi voter

--- src/validations/voter.validation.ts
+++ src/validations/voter.validation.ts
@@ -3,7 +3,7 @@ import zod, {z} from 'zod';
 const voterDTO = zod.object({
     name: z.string().trim().min(2).max(100),
     nik: z.string().trim().max(16).optional(),
-    info: z.string().trim().min(1).max(100).optional()
+    info: z.string().trim().max(100).optional()
 });
 
 export {voterDTO};
